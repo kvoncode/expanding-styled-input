1ec3dde89bb19a913817d56625f49a024e42c077
kvoncode <kvoncode@gmail.com>
2019-09-01 14:48:26 +0900

    add: shrinking width

--- app.js
+++ app.js
@@ -4,31 +4,26 @@ $(document).ready(function() {
   // From below width choose the smallest
   const widthLimitConst = 300;
   const viewportWidth = $(window).width();
-  console.log("TCL: viewportWidth", viewportWidth);
 
   const widthLimit =
     widthLimitConst > viewportWidth ? viewportWidth : widthLimitConst;
 
-  const inputMargin =
-    $("#text-field").outerWidth(true) - $("#text-field").outerWidth();
+  const initialInputWidth = $("#text-field").outerWidth(true);
 
-  console.log("TCL: inputMargin", inputMargin);
   // each time on keypress do following
   $("#text-field").keyup(function(e) {
     let inputText = $("#text-field").val();
 
     $("#width-measure").text(inputText);
 
-    let newWidth = $("#width-measure").outerWidth();
+    let newWidth = $("#width-measure").outerWidth(true);
 
-    if ($("#text-field").outerWidth(true) < widthLimit) {
-      
-      let oldWidth = $("#text-field").width();
 
-      if (newWidth > oldWidth) {
-        $("#text-field").width(newWidth);
-        console.log("TCL: newWidth", newWidth);
-      }
+    // compare outerWidth, but assign width
+
+    if (newWidth < widthLimit && newWidth > initialInputWidth) {
+      $("#text-field").width($("#width-measure").width());
+      console.log("TCL: newWidth", newWidth);
     }
   });
 });
